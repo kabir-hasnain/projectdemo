a861cb1619edd89ae775960907369d02c5590dcc
Hasnain Kabir <39668616+kabir-hasnain@users.noreply.github.com>
2022-07-26 23:05:24 +0600

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,25 +1,30 @@
 <?php
-	$firstName = $_POST['firstName'];
-	$lastName = $_POST['lastName'];
-	$gender = $_POST['gender'];
-	$email = $_POST['email'];
-	$password = $_POST['password'];
-	$confirmpassword = $_POST['confirmpassword'];
-	$number = $_POST['number'];
-	$department = $_POST['number'];
+    $firstName = $_POST['firstName'];
+    $lastName = $_POST['lastName'];
+    $gender = $_POST['gender'];
+    $email = $_POST['email'];
+    $department = $_POST['department'];
+    $password = $_POST['password'];
+    $confirmpassword = $_POST['confirmpassword'];
+    $number = $_POST['number'];
+    
 
-	// Database connection
-	$conn = new mysqli('localhost','root','','test');
-	if($conn->connect_error){
-		echo "$conn->connect_error";
-		die("Connection Failed : ". $conn->connect_error);
-	} else {
-		$stmt = $conn->prepare("insert into registration(firstName, lastName, gender, email, password, confirmpassword, number, department) values(?, ?, ?, ?, ?, ?, ?, ?)");
-		$stmt->bind_param("ssssssis", $firstName, $lastName, $gender, $email, $password, $confirmpassword, $number, $department);
-		$execval = $stmt->execute();
-		echo $execval;
-		echo "The form has been submitted";
-		$stmt->close();
-		$conn->close();
-	}
+    // Database connection
+   $conn = mysqli_connect('localhost','root','','kyau') or die("Connection Failed");
+   if($password == $confirmpassword){
+   $sql = "INSERT INTO registration (firstName,lastName,gender,email,department,password,confirm_password,number) VALUES ('$firstName','$lastName','$gender','$email','$department','$password','$confirmpassword','$number')";
+    $result = mysqli_query($conn, $sql) or die("Query Unsuccessful");
+
+        if($result){
+            echo "Form submitted successfully";
+        }else{
+            echo "Form submition failed";
+        }
+
+      }else{
+        echo "password and confirm_password mismatch";
+    }
+
+    
+   mysqli_close($conn); 
 ?>
